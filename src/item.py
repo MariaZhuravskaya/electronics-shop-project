e957b94eb39e150126aa1565f4bddcb149ea19ef
Maria <maria.mandrykina@gmail.com>
2023-06-16 11:16:02 +0300

    commit item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 
 class Item:
     """
@@ -5,7 +7,6 @@ class Item:
     """
     pay_rate = 0.8
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -15,11 +16,25 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_len):
+        """
+        Проверяет, что длина наименования товара не больше 10 симвовов. В противном случае, обрезать строку (оставить первые 10 символов)
+        """
+        if len(name_len) > 10:
+            # print(Exception('Длина наименования товара превышает 10 символов'))
+            self.__name = name_len[:10]
+        else:
+            self.__name = name_len
 
     def calculate_total_price(self) -> float:
         """
@@ -29,12 +44,27 @@ class Item:
         """
         return self.price * self.quantity
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Инициализируюет экземпляры класса `Item` данными из файла _src/items.csv
+        """
+        cls.all.clear()
+        with open('../src/items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                a = cls(row['name'], row['price'], row['quantity'])
+            return cls.all
 
-
+    @staticmethod
+    def string_to_number(string):
+        """
+        Статический метод, возвращающий число из числа-строки
+        """
+        return int(float(string))
